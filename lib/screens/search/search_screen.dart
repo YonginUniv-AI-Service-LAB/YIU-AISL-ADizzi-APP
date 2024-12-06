94e371a3b6d5def7872bc59b1139ec15f6f38e63
dkswndus <dkswndus6988@gmail.com>
2024-12-07 00:22:24 +0900

    :chore 서치 스크롤시 앱바 색상 변경 수정

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -26,20 +26,28 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
-        backgroundColor: Colors.white,
-          title: const Text('검색어를 입력해주세요'),
-        titleTextStyle: const TextStyle(color: Colors.black54,fontSize: 17),
-          titleSpacing: 0,
-
-          actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-            },
-
+          scrolledUnderElevation: 0, // 스크롤 할 때 앱바 색상 변경되지 않도록
+          backgroundColor: Colors.white,
+          title: const TextField(
+            autofocus: true, // 화면이 열리면 자동으로 키보드가 올라오게 설정
+            decoration: InputDecoration(
+              hintText: '검색어를 입력해주세요',
+              border: InputBorder.none,
+              hintStyle: TextStyle(color: Colors.black54, fontSize: 17),
+            ),
+            style: TextStyle(color: Colors.black87),
           ),
-        ],
+          titleSpacing: 0,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                // 검색 기능 처리
+              },
+            ),
+          ],
         shape: const Border(
           bottom: BorderSide(
             color: Color(0xFF5DDA6F),
